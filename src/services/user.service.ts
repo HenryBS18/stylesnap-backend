9a4e0111cc4a62ed56b52033634d79236e4709bc
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-12-12 18:01:52 +0800

    Create User service

---
+++ src/services/user.service.ts
@@ -0,0 +1,78 @@
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { UserRepo } from "repositories/user.repo";
+import { User } from "types";
+
+const userRepo: UserRepo = new UserRepo()
+const secret: string = process.env.JWT_SECRET!
+const expiresIn: number = 60 * 60 * 24 * 30
+
+interface RegisterData {
+  firstName: string
+  lastName?: string
+  email: string
+  password: string
+}
+
+export class UserService {
+  public async register(data: RegisterData): Promise<string> {
+    const { firstName, lastName = '', email, password } = data
+
+    const existedUser: User | null = await userRepo.findByEmail(email)
+
+    if (existedUser == null) {
+      throw new Error('Email is already used')
+    }
+
+    const hashedPassword: string = await bcrypt.hash(password, 10)
+
+    const user: User = await userRepo.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword
+    })
+
+    const payload = {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email
+    }
+
+    const token: string = jwt.sign(payload, secret, {
+      algorithm: 'HS256',
+      expiresIn: expiresIn
+    })
+
+    return token
+  }
+
+  public async login(email: string, password: string): Promise<String> {
+    const user: User | null = await userRepo.findByEmail(email)
+
+    if (user == null) {
+      throw new Error('Email not found')
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordValid) {
+      throw new Error('Wrong password')
+    }
+
+    const payload = {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email
+    }
+
+    const token: string = jwt.sign(payload, secret, {
+      algorithm: 'HS256',
+      expiresIn: expiresIn
+    })
+
+    return token
+  }
+}
