b52c9c1e55d4ee491fc619a344ff2f491bc2e04c
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-12-12 22:21:53 +0800

    Create User route

---
+++ src/routers/user.router.ts
@@ -0,0 +1,42 @@
+import express, { Router, Request, Response } from 'express'
+import { UserService } from 'services'
+
+const userService: UserService = new UserService()
+export const userRouter: Router = express.Router()
+
+userRouter.post('/register', async (req: Request, res: Response) => {
+  try {
+    const { firstName, lastName, email, password } = req.body
+
+    const token: string = await userService.register({
+      firstName,
+      lastName,
+      email,
+      password
+    })
+
+    res.cookie('session', token).status(200)
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(400).send({
+        error: error.message
+      })
+    }
+  }
+})
+
+userRouter.post('/login', async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body
+
+    const token: string = await userService.login(email, password)
+
+    res.cookie('session', token).status(200)
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(400).send({
+        error: error.message
+      })
+    }
+  }
+})
