deae85eedb52dd6455d8e95cd61bf79747e2b321
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-12-13 10:46:14 +0800

    Remove field from Clothes model

---
+++ prisma/migrations/20241213024232_remove_subtype_from_clothes/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `subtype` on the `Clothes` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `Clothes` DROP COLUMN `subtype`;
