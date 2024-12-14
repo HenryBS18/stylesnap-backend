9a320cd9eaaaf514e29e17e81512e63359047a59
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-12-15 00:14:08 +0800

    Add param type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express'
+import express, { Application, Request, Response } from 'express'
 import { clothesRouter, authRouter } from './routers'
 import { authMiddleware } from './middlewares/auth.middleware'
 
@@ -10,8 +10,8 @@ app.use(express.json())
 app.use('/api/', authRouter)
 app.use('/api/clothes', authMiddleware, clothesRouter)
 
-app.use('*', (req, res) => {
+app.use('*', (req: Request, res: Response) => {
   res.status(404).send('Route not found')
 })
 
-app.listen(port, () => console.log(`Server up and running on: http://localhost:${port}`));
+app.listen(port, () => console.log(`Server up and running on: http://localhost:${port}`))
