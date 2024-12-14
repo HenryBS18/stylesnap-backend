332bab6d96e1d34f5e06cced9a1371202a6c4105
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-12-14 21:49:30 +0800

    Create Auth middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt, { JwtPayload } from 'jsonwebtoken'
+
+const secret = process.env.JWT_SECRET!
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers.cookie?.replace('session=', '')
+
+  if (!token) {
+    res.status(401).send({
+      message: 'Token is required'
+    })
+    return
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, secret, { algorithms: ['HS256'] }) as JwtPayload
+    req.token = decodedToken
+    next()
+  } catch (error) {
+    res.status(401).send({
+      message: 'Invalid token'
+    })
+    return
+  }
+}
