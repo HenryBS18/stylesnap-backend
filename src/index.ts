e8b4be90354a9d487357bbdbe55964db624ca85e
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-12-15 20:19:07 +0800

    Adjust router prefix

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
 import express, { Application, Request, Response } from 'express'
-import { clothesRouter, authRouter, outfitRouter } from './routers'
-import { authMiddleware } from './middlewares/auth.middleware'
+import { router } from './routers'
 
 const app: Application = express()
 const port: number = parseInt(process.env.PORT as string, 10)
 
 app.use(express.json())
 
-app.use('/api/', authRouter)
-app.use('/api/clothes', authMiddleware, clothesRouter)
-app.use('/api/outfit', authMiddleware, outfitRouter)
+app.use('/api', router)
 
 app.use('*', (req: Request, res: Response) => {
   res.status(404).send('Route not found')
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,5 +1,11 @@
+import { authMiddleware } from "../middlewares";
 import { authRouter } from "./auth.router";
 import { clothesRouter } from "./clothes.router";
 import { outfitRouter } from "./outfit.router";
+import { Router } from "express";
 
-export { authRouter, clothesRouter, outfitRouter }
+export const router: Router = Router()
+
+router.use('/', authRouter)
+router.use('/clothes', authMiddleware, clothesRouter)
+router.use('/outfit', authMiddleware, outfitRouter)
