1e2da12bda3de75d9eab54efafe579911396085a
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-12-15 19:20:55 +0800

    Move interface to other file

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,23 +1,11 @@
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { UserRepo } from '../repositories';
-import { User } from "../types";
+import { LoginData, RegisterData, User } from "../types";
 
 const userRepo: UserRepo = new UserRepo()
 const secret: string = process.env.JWT_SECRET!
 const expiresIn: number = 60 * 60 * 24 * 30
-
-interface RegisterData {
-  firstName: string
-  lastName?: string
-  email: string
-  password: string
-}
-
-interface LoginData {
-  email: string
-  password: string
-}
 
 export class AuthService {
   public async register(data: RegisterData): Promise<string> {
