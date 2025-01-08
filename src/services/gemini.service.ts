b4adcafc70a9069ed265b5b256a77f4fb537258e
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2025-01-08 15:49:36 +0800

    Add system instruction to gemini model

--- src/services/gemini.service.ts
+++ src/services/gemini.service.ts
@@ -1,18 +1,35 @@
-import { GoogleGenerativeAI, Content } from "@google/generative-ai"
+import { GoogleGenerativeAI } from "@google/generative-ai"
+import { CreateGeminiPromptData } from "../types"
 
 const key: string = process.env.GEMINI_API_KEY!
 const genAI: GoogleGenerativeAI = new GoogleGenerativeAI(key)
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
 
 export class GeminiService {
-  public async createPrompt(history: Content[], prompt: string): Promise<String> {
-    const chat = model.startChat({
-      history: history,
-    })
+  public async createPrompt({ clothes, prompt }: CreateGeminiPromptData): Promise<String> {
+    try {
+      const chat = model.startChat({
+        systemInstruction: {
+          role: 'user',
+          parts: [
+            {
+              text: 'i want you to choose me an outfit based on my clothes, i will give you my clothes list, give me the answer in json format like this: [{"id": <clothesId>,"name": <clothesName>, "type": <clothesType>}, {clothes: [{"id": <clothesId>,"name": <clothesName>, "type": <clothesType>}, ..., ...]. give the answer dont ask back to me'
+            },
+            {
+              text: clothes
+            }
+          ]
+        }
+      })
 
-    const result = await chat.sendMessage(prompt)
-    const response = result.response.text()
+      const result = await chat.sendMessage(prompt)
+      const response = result.response.text()
 
-    return response
+      return response
+    } catch (error) {
+      if (error instanceof Error) throw new Error(error.message)
+    }
+
+    throw new Error('oops')
   }
 }
