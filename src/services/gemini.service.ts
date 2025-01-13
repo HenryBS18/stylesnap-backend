89771e662982403d4f4f82dfd58e409b6cd3551f
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2025-01-13 11:31:32 +0800

    Create createImageDescription

--- src/services/gemini.service.ts
+++ src/services/gemini.service.ts
@@ -1,5 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai"
 import { CreateGeminiPromptData } from "../types"
+import { readFileSync } from "fs"
 
 const key: string = process.env.GEMINI_API_KEY!
 const genAI: GoogleGenerativeAI = new GoogleGenerativeAI(key)
@@ -16,7 +17,7 @@ export class GeminiService {
               text: 'i want you to choose me an outfit based on my clothes, i will give you my clothes list, give me the answer in json format like this: [{"id": <clothesId>,"name": <clothesName>, "type": <clothesType>}, {clothes: [{"id": <clothesId>,"name": <clothesName>, "type": <clothesType>}, ..., ...]. give the answer dont ask back to me'
             },
             {
-              text: clothes
+              text: clothes,
             }
           ]
         }
@@ -26,6 +27,26 @@ export class GeminiService {
       const response = result.response.text()
 
       return response
+    } catch (error) {
+      if (error instanceof Error) throw new Error(error.message)
+    }
+
+    throw new Error('oops')
+  }
+
+  public async createImageDescription(file: Express.Multer.File): Promise<string> {
+    try {
+      const content = await model.generateContent([
+        'Describe this clothes',
+        {
+          inlineData: {
+            data: Buffer.from(readFileSync(file.path)).toString('base64'),
+            mimeType: file.mimetype
+          }
+        }
+      ])
+
+      return content.response.text()
     } catch (error) {
       if (error instanceof Error) throw new Error(error.message)
     }
--- src/services/index.ts
+++ src/services/index.ts
@@ -4,5 +4,6 @@ import { OutfitService } from "./outfit.service";
 import { CollectionService } from "./collection.service";
 import { PlannerService } from "./planner.service";
 import { PromptService } from "./prompt.service";
+import { GeminiService } from "./gemini.service";
 
-export { AuthService, ClothesService, OutfitService, CollectionService, PlannerService, PromptService }
+export { AuthService, ClothesService, OutfitService, CollectionService, PlannerService, PromptService, GeminiService }
