4a327194b950880f8634848e2f5a1bc1e682ca4f
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2025-01-13 22:05:06 +0800

    Add cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express, { Application, Request, Response } from 'express'
 import { router } from './routers'
+import cors from 'cors'
 
 const app: Application = express()
 const port: number = parseInt(process.env.PORT as string, 10)
 
 app.use(express.json())
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}))
 
 app.use('/api', router)
 
