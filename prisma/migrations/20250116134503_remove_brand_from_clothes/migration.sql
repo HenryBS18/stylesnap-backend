da68a8dd216ff651ee2fd0e67f105e3801637c5c
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2025-01-16 21:50:16 +0800

    Remove brand from clothes model

---
+++ prisma/migrations/20250116134503_remove_brand_from_clothes/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `brand` on the `Clothes` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `Clothes` DROP COLUMN `brand`;
