41ced6b68b54f872e479b06b49496987f06bd86e
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2025-01-16 22:44:12 +0800

    Add cors origin, and create https server

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express, { Application, Request, Response } from 'express'
+import https from 'https'
+import fs from 'fs'
+import path from 'path'
 import { router } from './routers'
 import cors from 'cors'
 
 const app: Application = express()
 const port: number = parseInt(process.env.PORT as string, 10)
+const protocol: string = process.env.PROTOCOL as string
 
 app.use(express.json())
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://3l89513h-5173.asse.devtunnels.ms', 'https://10.1.51.98:5173', 'https://192.168.1.7:5173'],
   credentials: true
 }))
 
@@ -17,4 +21,13 @@ app.use('*', (req: Request, res: Response) => {
   res.status(404).send('Route not found')
 })
 
-app.listen(port, () => console.log(`Server up and running on: http://localhost:${port}`))
+if (protocol === 'http') {
+  app.listen(port, () => console.log(`Server up and running on: http://localhost:${port}`))
+} else {
+  https.createServer({
+    key: fs.readFileSync(path.resolve(__dirname, `${process.env.HOST}-key.pem`)),
+    cert: fs.readFileSync(path.resolve(__dirname, `${process.env.HOST}.pem`))
+  }, app).listen(port, () => {
+    console.log(`HTTPS server up and running on: https://localhost:${port}`);
+  })
+}
